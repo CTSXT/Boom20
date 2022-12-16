a9da54fa30e55b9f419cd933b7645b4901e8b583
David Haralambiev <david@davidharalambiev.tech>
2022-12-16 12:17:01 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,15 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  let pokemons;
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+  .then((response) => response.json())
+  .then((data) => pokemons=data;);
+  pokemons.forEach(elem=>{
+  let li=document.createElement('li');
+    li.innerHTML =elem;
+    ul.appendChild(li); 
+  })
+  
+  
 });
